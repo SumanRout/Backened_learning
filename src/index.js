1081f696709724566ae803461e483bc4336f4f7a
SumanRout <sumanrout824@gmail.com>
2025-08-20 15:22:50 +0530

    Utils(ApiError,Apiresponse)

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ dotenv.config({
     path:'../.env'
 
 })
-
+const port=process.env.PORT || 8000;
 connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`App is running at ${port}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDb connection failed !!",err)
+})
+
 
 
 
---
+++ src/utils/Apierror.js
@@ -0,0 +1,20 @@
+//File for apierror
+class ApiError extends Error{
+    constructor(statuscode,message="Something went Wrong",errors=[],stack=""){
+        super(message)
+        this.statuscode=statuscode;
+        this.data=null;
+        this.message=message;
+        this.success=false;
+        this.errors=errors;
+        if(stack){
+            this.stack=stack;
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+
+
+    }
+}
+export {ApiError}
